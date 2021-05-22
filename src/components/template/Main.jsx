7c03025e6377965102ecbee2a455ee90bfab9dd2
Caio Jardim <caiojardinho@gmail.com>
2021-05-22 17:00:22 -0300

    get the todo list in backend and show

--- src/components/template/Main.jsx
+++ src/components/template/Main.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import './Main.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function (props) {
+    const [listElements, setListElements] = useState([])
     
+    useEffect(() => {
+        axios.get('http://localhost:3001/todo')
+            .then(resp => setListElements(resp.data))
+    }, [])       
+
+        console.log(listElements)
     return (
-        <main>
-            Main
-        </main>
+        <div className="container">
+            <div className='container-element'>
+            <ul>
+            {listElements.map(item => {
+                return(
+                    <>
+                        <li key={item.id}>
+                            <span>{item.text}</span>
+                                <button>
+                                    <img src='trash-icon.png' width="20px" alt="Trash Icon"/>
+                                </button>
+                        </li>
+                        
+                    </>
+                )
+            })}
+            </ul>
+            </div>
+            <div className='container-element'>
+                <ul>
+                    <li>Test</li>
+                </ul>
+            </div>
+            <div className='container-element'>
+                <ul>
+                    <li>Test</li>
+                </ul>
+            </div>
+            
+        </div>
     )
 }
